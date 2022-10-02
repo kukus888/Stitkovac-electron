6194a6e37fcb0e0c27e6d72e59542611f921f739
kukus888 <jskora696@gmail.com>
2022-10-02 21:21:49 +0200

    Loading config on frontend

--- settingsRenderer.js
+++ settingsRenderer.js
@@ -7,9 +7,6 @@ document.getElementById("btnGoHome").addEventListener("click", () => {
 document.getElementById("btnLoadCfg").addEventListener("click", () => {
     ipcRenderer.send('load-config');
 });
-document.getElementById("btnAlignPrinter").addEventListener("click", () => {
-    alignPrintHead();
-});
 
 //create a div for error message when printer is not connected etc...
 let printerErrMsg = document.createElement('div')
@@ -36,7 +33,72 @@ window.electronAPI.handleSyncConfig((event, data) => {
     renderConfig();
 });
 
-function renderConfig(){
+function renderConfig() {
+    //clear all config options...
+    for (i = 0; i < document.getElementsByTagName("tr").length; i++) {
+        if (!document.getElementsByTagName("tr").item(i).id.includes("header")) {
+            document.getElementsByTagName("tr").item(i).remove();
+        }
+    }
+    //...and render them again
+    let settings = config.settings;
+    for (i = 0; i < settings.length; i++) {
+        let args = {
+            arg0: "",
+            opt0: "",
+            arg1: "",
+            opt1: ""
+        };
+        switch(settings[i].arguments.length){
+            case 0:
+                args.arg0 = "This has no arguments"
+                break;
+            case 1:
+                args.arg0 = Object.keys(settings[i].arguments[0]);
+                args.opt0 = Object.values(config.settings[0].arguments[0])[0];
+                break;
+            case 2:
+                args.arg0 = Object.keys(settings[i].arguments[0]);
+                args.opt0 = Object.values(config.settings[0].arguments[0])[0];
+                args.arg1 = Object.keys(settings[i].arguments[1]);
+                args.opt1 = Object.values(config.settings[0].arguments[1])[0];
+                break;
+        }
+        let id = `configItem${i}`;
+        let elementStr = `<td class="align-middle">${settings[i].name}</td>
+                    <td class="align-middle">
+                        ${args.arg0}
+                    </td>
+                    <td class="align-middle">
+                        <input class="input bg-transparent m-0 form-control" type="text" id="inputNewCN" value="${args.opt0}">
+                    </td>
+                    <td class="align-middle">
+                        ${args.arg1}
+                    </td>
+                    <td class="align-middle">
+                    <input class="input bg-transparent m-0 form-control" type="text" id="inputNewCN" value="${args.opt1}">
+                    </td>
+                    <td>
+                        <button class="btn btn-outline-secondary" id="btn${id}Save">Save</button>
+                        <button class="btn btn-outline-secondary" id="btn${id}Run">\>_ Run</button>
+                    </td>
+        `;
+        let newTr = document.createElement('tr');
+        newTr.id = id;
+        document.getElementById("datatableBody").appendChild(newTr);
+        document.getElementById(id).innerHTML += elementStr;
+
+        document.getElementById("btn${id}Run").addEventListener("click", () => {
+           RunConfigurationCommand(settings[i]);
+        });
+
+        document.getElementById("btn${id}Save").addEventListener("click", () => {
+            
+        });
+    }
+}
+
+function RunConfigurationCommand(setting){
 
 }
 
@@ -48,6 +110,6 @@ function alignPrintHead() {
     ipcRenderer.send('alignPrintHead', true);
 }
 
-function redirect(url){
+function redirect(url) {
     ipcRenderer.send('redirect', url);
 }
